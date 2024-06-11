a6cbadf50ba1be33ee358ac44753ef8395ffa7e7
Felipe Batista Gomes <felipe.gomes@ad.ort.org.br>
2024-06-11 11:09:26 -0300

    comitado

--- App.js
+++ App.js
@@ -1,20 +1,89 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Image,
+  Button,
+  Text,
+  TextInput,
+  Pressable,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.container2}>
+        <Text style={styles.titulo}>Login</Text>
+        <TextInput
+          placeholder="Digite seu email"
+          style={styles.input}
+        ></TextInput>
+        <TextInput
+          placeholder="Digite sua senha"
+          style={styles.input}
+        ></TextInput>
+        <Text>Esqueceu sua senha?</Text>
+        <Pressable style={styles.botao}>
+          <Text style={styles.buttxt}>Entrar</Text>
+        </Pressable>
+      </View>
+      <View style={styles.container2}>
+        <Pressable
+          style={{
+            alignItems: "center",
+            width: 300,
+            marginTop: 25,
+            padding: 5,
+            borderRadius: 5,
+            backgroundColor: "white",
+            text: "black",
+            padding: 10,
+            borderWidth: 1,
+          }}
+        >
+          <Text style={{ backgroundColor: "white" }}>
+            Não tem uma conta?
+            <Text style={{ color: "red" }}> Registre-se!</Text>
+          </Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 250,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  container2: {
+    marginTop: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  titulo: {
+    fontSize: 20,
+  },
+  input: {
+    width: 300,
+    marginTop: 25,
+    padding: 5,
+    borderWidth: 1,
+    borderColor: "black",
+    borderRadius: 5,
+  },
+  botao: {
+    alignItems: "center",
+    width: 300,
+    marginTop: 25,
+    padding: 5,
+    borderRadius: 5,
+    backgroundColor: "red",
+    text: "white",
+    padding: 10,
+  },
+  buttxt: {
+    color: "white",
   },
 });
