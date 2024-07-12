d48a839504e490d79a4487a17b9a59e606f30046
Felipe-Batista-Gomes <felipe.gomes@ort.org.br>
2024-07-12 16:34:37 -0300

    12/07/2024

--- src/screens/Perfil/index.js
+++ src/screens/Perfil/index.js
@@ -8,6 +8,8 @@ import {
   TextInput,
   Pressable,
   Input,
+  ScrollView,
+  SafeAreaView,
 } from "react-native";
 
 import { useNavigation } from "@react-navigation/native";
@@ -15,46 +17,51 @@ import { useNavigation } from "@react-navigation/native";
 export default function Perfil() {
   const navigation = useNavigation();
   return (
-    <View>
-      <View style={styles.container}>
-        <View style={styles.container2}></View>
-      </View>
+    <View style={{ flex: 1 }}>
+      <ScrollView>
+        <View style={styles.container}>
+          <View style={styles.container2}></View>
+        </View>
 
-      <Text style={{ marginTop: 60, marginLeft: 40, fontSize: 30 }}>
-        Seu nome
-      </Text>
+        <Text style={{ marginTop: 60, marginLeft: 40, fontSize: 30 }}>
+          Seu nome
+        </Text>
 
-      <Text
-        style={{
-          marginTop: 60,
-          fontSize: 20,
-          height: 300,
-          borderWidth: 3,
-          borderColor: "black",
-          padding: 20,
-        }}
-      >
-        Sua descrição
-      </Text>
+        <Text
+          style={{
+            marginTop: 60,
+            marginLeft: 30,
+            marginRight: 30,
+            fontSize: 20,
+            height: 300,
+            borderWidth: 3,
+            borderColor: "black",
+            padding: 10,
+          }}
+        >
+          Sua descrição
+        </Text>
 
-      <Text
-        style={{
-          marginTop: 60,
-          fontSize: 20,
-          height: 500,
-          borderWidth: 3,
-          borderColor: "black",
-          padding: 20,
-        }}
-      >
-        Sua descrição2
-      </Text>
+        <Text
+          style={{
+            marginTop: 60,
+            fontSize: 20,
+            height: 300,
+            borderWidth: 3,
+            borderColor: "black",
+            padding: 20,
+          }}
+        >
+          Sua descrição2
+        </Text>
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     backgroundColor: "orange",
     height: 300,
     width: "100%",
